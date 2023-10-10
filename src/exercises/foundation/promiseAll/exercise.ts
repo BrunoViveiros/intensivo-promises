32f99be325766d1072779a1c228c0266dd7ba31e
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2023-10-10 17:21:03 -0300

    Exercise - Foundation 1 - Promise.all

--- src/exercises/foundation/promiseAll/exercise.ts
+++ src/exercises/foundation/promiseAll/exercise.ts
@@ -1,1 +1,23 @@
-export default async <T>(promises: Array<Promise<T>>): Promise<Array<T>> => {};
+export default async <T>(promises: Array<Promise<T>>): Promise<Array<T>> => {
+  const lengthWithoutEmpty = (arr: unknown[]) => {
+    return arr.filter((i) => i !== undefined).length;
+  };
+
+  return new Promise((resolve, reject) => {
+    const allValues = [] as T[];
+
+    promises.forEach((promise, index) => {
+      promise
+        .then((response) => {
+          allValues[index] = response;
+
+          if (lengthWithoutEmpty(allValues) === promises.length) {
+            resolve(allValues);
+          }
+        })
+        .catch((e) => {
+          reject(e);
+        });
+    });
+  });
+};
