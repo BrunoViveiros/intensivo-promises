33c780feed9185fb0fd7ba00e3a5b4a727f840fd
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2023-10-10 15:46:58 -0300

    Exercise - Concrete 6 - Parallel Chuncks

--- src/exercises/concrete/parallelChunks/exercise.ts
+++ src/exercises/concrete/parallelChunks/exercise.ts
@@ -5,4 +5,30 @@ type Context = {
 };
 
 export default ({ postData }: Context) =>
-  async (list: Array<string>) => {};
+  async (list: Array<string>) => {
+    const createParallelChunk = (list: Array<string>) => {
+      const allPromises = list.map((item) => postData(item));
+
+      return Promise.all(allPromises);
+    };
+
+    const createParallelChunckList = async (list: Array<string>) => {
+      const newList = [...list];
+      const allPromises = [];
+
+      while (newList.length > 0) {
+        const currentChunk = newList.splice(0, 5);
+        const currentChunkResults = await createParallelChunk(currentChunk);
+
+        allPromises.push(currentChunkResults);
+      }
+
+      return allPromises.flatMap((i) => i);
+    };
+
+    return await createParallelChunckList(list);
+  };
+
+// - Signature: `(list: Array<string>) => Promise<Array<string>>`
+// - Calls `postData` with each element in `list` in parallel, in chunks of 5
+// - Returns the list of results of `postData`
