0670a087ea4257a84dc2b28dc7b8ec87fe8a4153
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2023-10-10 17:28:47 -0300

    Exercise - Foundation 2 - Promise.allSettled

--- src/exercises/foundation/promiseAllSettled/exercise.ts
+++ src/exercises/foundation/promiseAllSettled/exercise.ts
@@ -1,1 +1,30 @@
-export default async <T>(promises: Array<Promise<T>>) => {};
+export default async <T>(promises: Array<Promise<T>>) => {
+  const lengthWithoutEmpty = (arr: unknown[]) => {
+    return arr.filter((i) => i !== undefined).length;
+  };
+
+  type Fulfilled = { status: "fulfilled"; value: T };
+  type Rejected = { status: "rejected"; reason: unknown };
+  type Data = [Fulfilled | Rejected];
+
+  return new Promise((resolve) => {
+    const allValues = [] as unknown as Data;
+
+    promises.forEach((promise, index) => {
+      promise
+        .then((response) => {
+          allValues[index] = { status: "fulfilled", value: response };
+
+          if (lengthWithoutEmpty(allValues) === promises.length) {
+            resolve(allValues);
+          }
+        })
+        .catch((e) => {
+          allValues[index] = { status: "rejected", reason: e };
+          if (lengthWithoutEmpty(allValues) === promises.length) {
+            resolve(allValues);
+          }
+        });
+    });
+  });
+};
