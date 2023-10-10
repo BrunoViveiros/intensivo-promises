80aef600a38ad61a470eaf67595bdcce756e3134
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2023-10-10 14:14:57 -0300

    Exercise - Concrete 1 - Serial

--- src/exercises/concrete/serial/exercise.ts
+++ src/exercises/concrete/serial/exercise.ts
@@ -3,4 +3,25 @@ type Context = {
 };
 
 export default ({ postData }: Context) =>
-  async (list: Array<string>) => {};
+  async (list: Array<string>) => {
+    const withFor = async () => {
+      const allData = [];
+
+      for (const item of list) {
+        const data = await postData(item);
+        allData.push(data);
+      }
+
+      return allData;
+    };
+
+    const withReduce = async () => {
+      return list.reduce(async (acc, item) => {
+        const lastPromise = await acc;
+        const data = await postData(item);
+        return [...lastPromise, data];
+      }, Promise.resolve([] as string[]));
+    };
+
+    return await withReduce();
+  };
