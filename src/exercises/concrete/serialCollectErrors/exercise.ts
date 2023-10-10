a07def7efe4858d016a2294fcba48c031076906a
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2023-10-10 15:00:19 -0300

    Exercise - Concrete 4 - Serial Collect Errors

--- src/exercises/concrete/serialCollectErrors/exercise.ts
+++ src/exercises/concrete/serialCollectErrors/exercise.ts
@@ -3,4 +3,45 @@ type Context = {
 };
 
 export default ({ postData }: Context) =>
-  async (list: Array<string>) => {};
+  async (list: Array<string>) => {
+    type Data = {
+      successes: string[];
+      errors: string[];
+    };
+
+    const withFor = async () => {
+      const data: Data = {
+        successes: [],
+        errors: [],
+      };
+
+      for (const item of list) {
+        try {
+          const response = await postData(item);
+          data.successes.push(response);
+        } catch (error) {
+          data.errors.push(String(error));
+        }
+      }
+
+      return data;
+    };
+
+    const withReduce = async () => {
+      return list.reduce(async (acc, item) => {
+        const response = await acc;
+
+        try {
+          const data = await postData(item);
+          return {
+            ...response,
+            successes: [...response.successes, data],
+          };
+        } catch (error) {
+          return { ...response, errors: [...response.errors, String(error)] };
+        }
+      }, Promise.resolve({ successes: [], errors: [] } as Data));
+    };
+
+    return withReduce();
+  };
