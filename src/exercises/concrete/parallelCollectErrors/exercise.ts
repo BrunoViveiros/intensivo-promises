e9aba3e815df949f8daca223f69200f569d94eae
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2023-10-10 15:13:15 -0300

    Exercise - Concrete 5 - Parallel Collect Errors

--- src/exercises/concrete/parallelCollectErrors/exercise.ts
+++ src/exercises/concrete/parallelCollectErrors/exercise.ts
@@ -3,4 +3,28 @@ type Context = {
 };
 
 export default ({ postData }: Context) =>
-  async (list: Array<string>) => {};
+  async (list: Array<string>) => {
+    type Data = {
+      successes: string[];
+      errors: string[];
+    };
+
+    const data: Data = {
+      successes: [],
+      errors: [],
+    };
+
+    const mapResponseWithReduce = (values: PromiseSettledResult<string>[]) => {
+      return values.reduce((acc, response) => {
+        if (response.status === "fulfilled")
+          return { ...acc, successes: [...acc.successes, response.value] };
+        if (response.status === "rejected")
+          return { ...acc, errors: [...acc.errors, response.reason] };
+        return acc;
+      }, data);
+    };
+
+    const allPromises = list.map((item) => postData(item));
+
+    return Promise.allSettled(allPromises).then(mapResponseWithReduce);
+  };
