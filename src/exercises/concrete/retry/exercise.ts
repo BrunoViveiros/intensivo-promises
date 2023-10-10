6aaf473b53825fe491033a99f5c2f86da9078cf3
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2023-10-10 14:53:52 -0300

    Exercise - Concrete 3 - Retry

--- src/exercises/concrete/retry/exercise.ts
+++ src/exercises/concrete/retry/exercise.ts
@@ -3,4 +3,24 @@ type Context = {
 };
 
 export default ({ getData }: Context) =>
-  async (data: string) => {};
+  async (data: string) => {
+    const retryTimes = async (times: number) => {
+      const errors = [];
+
+      for (let i = 0; i <= times; i++) {
+        try {
+          const response = await getData(data);
+
+          return response;
+        } catch (error) {
+          errors.push(error);
+
+          if (i === times) {
+            throw errors;
+          }
+        }
+      }
+    };
+
+    return retryTimes(3);
+  };
