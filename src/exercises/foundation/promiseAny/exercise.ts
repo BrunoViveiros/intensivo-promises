b47eaff4099f4f8f896f0f88025f159ff9a8366d
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2023-10-11 09:38:28 -0300

    Exercise - Foundation 3 - Promise.any

--- src/exercises/foundation/promiseAny/exercise.ts
+++ src/exercises/foundation/promiseAny/exercise.ts
@@ -1,1 +1,22 @@
-export default async <T>(promises: Array<Promise<T>>): Promise<T> => {};
+export default async <T>(promises: Array<Promise<T>>): Promise<T> => {
+  const lengthWithoutEmpty = (arr: unknown[]) => {
+    return arr.filter((i) => i !== undefined).length;
+  };
+
+  const allErrors: string[] = [];
+
+  return new Promise((resolve, reject) => {
+    promises.forEach((promise, index) => {
+      promise
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          allErrors[index] = error;
+          if (lengthWithoutEmpty(allErrors) === promises.length) {
+            reject(allErrors);
+          }
+        });
+    });
+  });
+};
